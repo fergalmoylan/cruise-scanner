f9b59d99b876acf52c78f6640d05715e310eb6ac
Fergal Moylan <47277854+fergalmoylan@users.noreply.github.com>
2025-11-22 21:05:40 +0000

    Use docs directory for github pages (#6)

--- src/main.py
+++ src/main.py
@@ -36,8 +36,8 @@ def main():
     parser.add_argument(
         "--csv-output",
         type=str,
-        default="data/cruise_prices_v2.csv",
-        help="Output CSV file path (default: data/cruise_prices_v2.csv)",
+        default="docs/cruise_prices_v2.csv",
+        help="Output CSV file path (default: docs/cruise_prices_v2.csv)",
     )
 
     args = parser.parse_args()
